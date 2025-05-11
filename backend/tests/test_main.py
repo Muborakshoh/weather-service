99c8d7f518d62bdfa0acf03cadc433dc18d45616
Muboraksho Inoyatullo <muborakshoinoyatullo@MacBook-Pro-Muboraksho.local>
2025-05-11 15:11:50 +0300

    CI/CD in Github Actions

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,24 +1,8 @@
-import pytest
 from fastapi.testclient import TestClient
-from main import app
+from main import app  # Убедитесь, что main.py доступен
 
-def test_example():
-    assert True
+client = TestClient(app)
 
-'''client = TestClient(app)
-
-def test_get_weather_success():
-    """Тест успешного получения погоды для существующего города."""
-    response = client.get("/weather/London")
+def test_read_main():
+    response = client.get("/")
     assert response.status_code == 200
-    json_response = response.json()
-    assert "city" in json_response
-    assert "temperature" in json_response
-    assert "description" in json_response
-    assert json_response["city"] == "London"
-
-def test_get_weather_not_found():
-    """Тест обработки несуществующего города."""
-    response = client.get("/weather/NonExistentCity")
-    assert response.status_code == 404
-    assert response.json() == {"detail": "City not found"}'''
